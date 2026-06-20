c916cb8befeea2a726390963c246a7bc428b67b0
apliraa <arthur.p.lira@gmail.com>
2026-06-20 15:31:41 -0300

    feat: adição de um placeholder de música de fundo inicial

---
+++ AudioManager.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class AudioManager : AudioStreamPlayer2D
+{
+	
+	//para trocar de música no futuro durante alguma fase ou sla
+  //  public void TrocarMusica(AudioStream novaMusica)
+ //   {
+	 //   Stream = novaMusica;
+  //      Play();
+	//}
+
+	
+	
+}
